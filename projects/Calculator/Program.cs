61ca358e93f0588675e1efbceffb7fa953793aa2
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-08-31 20:52:32 -0300

    feat: Projeto Calculadora - Iniciando o projeto

---
+++ projects/Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear(); // Limpa a tela
+
+      Console.WriteLine("Primeiro valor: ");
+      float valor1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine(valor1);
+    }
+  }
+}
