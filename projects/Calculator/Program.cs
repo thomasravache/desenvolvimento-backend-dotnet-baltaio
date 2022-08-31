8cdd9c2677d27d827cdd89b37728542b5c443e37
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-08-31 20:58:59 -0300

    feat: Projeto Calculadora - Soma

--- projects/Calculator/Program.cs
+++ projects/Calculator/Program.cs
@@ -11,7 +11,18 @@ namespace Calculator
       Console.WriteLine("Primeiro valor: ");
       float valor1 = float.Parse(Console.ReadLine());
 
-      Console.WriteLine(valor1);
+      Console.WriteLine("Segundo valor:");
+      float valor2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = valor1 + valor2;
+      Console.WriteLine("O resultado da soma é " + resultado);
+      // Console.WriteLine("O resultado da soma é " + (valor1 + valor2));
+
+      Console.WriteLine($"O resultado da some é {resultado} - (exibindo com interpolação)");
+      // Console.WriteLine($"O resultado da some é {valor1 + valor2} - (exibindo com interpolação)");
+
     }
   }
 }
