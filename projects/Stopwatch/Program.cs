92ca3bfa13b7525aa035900d9681e4baf793260d
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-09-27 20:18:42 -0300

    feat: Projeto Stopwatch - opçoes do menu

--- projects/Stopwatch/Program.cs
+++ projects/Stopwatch/Program.cs
@@ -12,10 +12,12 @@ namespace Stopwatch
     static void Menu()
     {
       Console.Clear();
-      Console.WriteLine("S = Segundo");
-      Console.WriteLine("M = Minuto");
+      Console.WriteLine("S = Segundo => 10s = 10 segundos");
+      Console.WriteLine("M = Minuto => 1m = 1 minuto");
       Console.WriteLine("0 = Sair");
       Console.WriteLine("Quanto tempo deseja contar?");
+
+      string data = Console.ReadLine().ToLower();
     }
 
     static void Start(int time)
