f18ecb9688fc43aee807ffd4b754f3c04f39b5d8
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-10-16 23:35:32 -0300

    Projeto Stopwatch - Calculando tempo

--- projects/Stopwatch/Program.cs
+++ projects/Stopwatch/Program.cs
@@ -22,9 +22,16 @@ namespace Stopwatch
       char type = char.Parse(data.Substring(data.Length - 1, 1));
 
       int time = int.Parse(data.Substring(0, data.Length - 1));
+      int multiplier = 1;
 
-      Console.WriteLine(type);
-      Console.WriteLine(time);
+      if (type == 'm')
+      {
+        multiplier = 60;
+      }
+
+      if (time == 0) System.Environment.Exit(0);
+
+      Start(time * multiplier);
     }
 
     static void Start(int time)
@@ -42,6 +49,7 @@ namespace Stopwatch
       Console.Clear();
       Console.WriteLine("Stopwatch finalizado");
       Thread.Sleep(2500);
+      Menu();
     }
   }
 }
