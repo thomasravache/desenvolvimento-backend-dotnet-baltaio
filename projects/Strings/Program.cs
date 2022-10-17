0208ed59cad56dfeccbbaf3ce22354962932bb0b
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-10-17 19:33:47 -0300

    feat: strings - guids

---
+++ projects/Strings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Strings
+{
+  public static class Program
+  {
+    static void Main(string[] args)
+    {
+      var id = Guid.NewGuid();
+      // convertendo Guid pra string
+      id.ToString();
+
+      // id = new Guid("") -> passar um guid valido para gerar um guid
+      Console.WriteLine(id);
+    }
+  }
+}
