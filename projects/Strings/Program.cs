76e2896c4a0612bf5a45b14665b4c7c11e6b7a71
Thomas Ravache <thomasravache31@gmail.com>
2022-10-19 02:05:43 -0300

    feat: strings - equals

--- projects/Strings/Program.cs
+++ projects/Strings/Program.cs
@@ -7,14 +7,13 @@ namespace Strings
     static void Main(string[] args)
     {
       var texto = "Este texto é um teste";
+      var numero = 10;
 
-      Console.WriteLine(texto.StartsWith("Este"));
-      Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase));
-      Console.WriteLine(texto.StartsWith("texto"));
-
-      Console.WriteLine(texto.EndsWith("teste"));
-      Console.WriteLine(texto.EndsWith("Teste"));
-      Console.WriteLine(texto.EndsWith("xpro"));
+      numero.Equals(5);
+      numero.Equals(10);
+      Console.WriteLine(texto.Equals("Este texto é um teste"));
+      Console.WriteLine(texto.Equals("este texto é um teste"));
+      Console.WriteLine(texto.Equals("este texto é um teste", StringComparison.OrdinalIgnoreCase));
     }
   }
 }
