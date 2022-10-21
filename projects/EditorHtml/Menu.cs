ce75a1f4ba058808b7246fbee9fa21d5a39bb959
Thomas Ravache <thomasravache31@gmail.com>
2022-10-21 02:54:04 -0300

    feat: projeto editorHtml - iniciando projeto

---
+++ projects/EditorHtml/Menu.cs
@@ -0,0 +1,14 @@
+namespace EditorHtml
+{
+  public static class Menu
+  {
+    public static void Show()
+    {
+      Console.Clear();
+      Console.BackgroundColor = ConsoleColor.Blue;
+      Console.ForegroundColor = ConsoleColor.Black;
+
+      
+    }
+  }
+}
