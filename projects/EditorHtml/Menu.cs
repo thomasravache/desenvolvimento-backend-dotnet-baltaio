4517d87e0ec2db7d1004ae50bc1c1f08e60f1601
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-10-27 22:49:03 -0300

    feat: projeto editor html - exibindo menu

--- projects/EditorHtml/Menu.cs
+++ projects/EditorHtml/Menu.cs
@@ -9,6 +9,9 @@ namespace EditorHtml
       Console.ForegroundColor = ConsoleColor.Black;
 
       DrawScreen(10, 30);
+      WriteOptions();
+
+      var option = short.Parse(Console.ReadLine());
     }
 
     public static void DrawScreen(int lines, int columns)
@@ -37,6 +40,25 @@ namespace EditorHtml
 
         Console.Write("\n");
       }
+    }
+
+    public static void WriteOptions()
+    {
+      Console.SetCursorPosition(3, 2);
+      Console.WriteLine("Editor HTML");
+      Console.SetCursorPosition(3, 3);
+      Console.WriteLine("=======");
+      Console.SetCursorPosition(3, 4);
+      Console.WriteLine("Selecione uma opção abaixo");
+      Console.SetCursorPosition(3, 6);
+      Console.WriteLine("1 - Novo Arquivo");
+      Console.SetCursorPosition(3, 7);
+      Console.WriteLine("2 - Abrir");
+      Console.SetCursorPosition(3, 9);
+      Console.WriteLine("0 - Sair");
+      Console.SetCursorPosition(3, 10);
+
+      Console.Write("Opção: ");
     }
   }
 }
