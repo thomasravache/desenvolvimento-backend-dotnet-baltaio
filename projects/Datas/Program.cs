e862fa4ae469f8feb3697ad8edcd26600d64e028
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-11-14 08:25:21 -0300

    feat: datas - iniciando com datas

---
+++ projects/Datas/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Datas
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // var data = new DateTime(); criar data
+      var dataHoje = DateTime.Now;
+    }
+  }
+}
