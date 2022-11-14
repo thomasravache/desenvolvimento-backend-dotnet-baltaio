bf6ff9f2a742dc0cac5165f7a8f39e2c86aa0e48
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-11-14 08:48:44 -0300

    feat: datas - formatando datas

--- projects/Datas/Program.cs
+++ projects/Datas/Program.cs
@@ -6,16 +6,18 @@
     {
       Console.Clear();
 
-      var dataEspecifica = new DateTime(2020, 10, 12, 8, 23, 14);
+      var data = DateTime.Now;
 
-      Console.WriteLine(dataEspecifica);
-      Console.WriteLine(dataEspecifica.Year);
-      Console.WriteLine(dataEspecifica.Month);
-      Console.WriteLine(dataEspecifica.Hour);
-      Console.WriteLine(dataEspecifica.Minute);
-      Console.WriteLine(dataEspecifica.Second);
-      Console.WriteLine(dataEspecifica.DayOfWeek);
-      Console.WriteLine((int)dataEspecifica.DayOfWeek);
+      // Y = ano
+      // M = mes
+      // d = dia
+      // h = hora
+      // m = minuto
+      // s = segundo
+      // var formatada = String.Format("{0:yyyy}", data); // ano
+      // var formatada = String.Format("{0:yyyy-MM-dd}", data); //
+      var formatada = String.Format("{0:dd/MM/yyyy hh:mm:ss ff z}", data); // ff e zz para fração de segundo e timezone
+      Console.WriteLine(formatada);
     }
   }
 }
