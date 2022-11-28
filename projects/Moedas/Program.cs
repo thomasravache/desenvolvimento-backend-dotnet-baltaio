2d8767c157f3878212568235e3497cdb3102d2b6
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 20:40:24 -0300

    feat: Moedas - Tipo para moedas

---
+++ projects/Moedas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Moedas
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+
+      // Geralmente se usa decimal para armazenar moedas
+
+      decimal valor = 10.25m;
+      Console.WriteLine(valor);
+    }
+  }
+}
