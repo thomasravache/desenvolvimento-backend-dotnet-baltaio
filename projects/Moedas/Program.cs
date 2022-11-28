ddb8c0fcfbdb9236cbd4e5dfa5e316c6d0cc637c
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 20:56:15 -0300

    feat: Moedas - Formatando moedas

--- projects/Moedas/Program.cs
+++ projects/Moedas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Moedas
 {
@@ -10,8 +11,18 @@ namespace Moedas
 
       // Geralmente se usa decimal para armazenar moedas
 
-      decimal valor = 10.25m;
-      Console.WriteLine(valor);
+      decimal valor = 10526.25m;
+
+      Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("pt-BR")));
+      Console.WriteLine(String.Format("R$ {0}", valor.ToString(CultureInfo.CreateSpecificCulture("pt-BR"))));
+
+      // Formatando como moeda ou número
+      Console.WriteLine(valor.ToString("G", CultureInfo.CreateSpecificCulture("pt-BR"))); // número
+      Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); // moeda
+      Console.WriteLine(valor.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR"))); // formata como moeda sem R$
+      Console.WriteLine(valor.ToString("P", CultureInfo.CreateSpecificCulture("pt-BR"))); // porcentagem
+      Console.WriteLine(valor.ToString("E04", CultureInfo.CreateSpecificCulture("pt-BR")));
+      Console.WriteLine(valor.ToString("F", CultureInfo.CreateSpecificCulture("pt-BR")));
     }
   }
 }
