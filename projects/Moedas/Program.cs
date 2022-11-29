66efead8986e81d60c5f314d1c5f77e0fc425fe9
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 21:00:11 -0300

    feat: Moedas - Math

--- projects/Moedas/Program.cs
+++ projects/Moedas/Program.cs
@@ -9,20 +9,11 @@ namespace Moedas
     {
       Console.Clear();
 
-      // Geralmente se usa decimal para armazenar moedas
-
       decimal valor = 10526.25m;
 
-      Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("pt-BR")));
-      Console.WriteLine(String.Format("R$ {0}", valor.ToString(CultureInfo.CreateSpecificCulture("pt-BR"))));
-
-      // Formatando como moeda ou número
-      Console.WriteLine(valor.ToString("G", CultureInfo.CreateSpecificCulture("pt-BR"))); // número
-      Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); // moeda
-      Console.WriteLine(valor.ToString("N", CultureInfo.CreateSpecificCulture("pt-BR"))); // formata como moeda sem R$
-      Console.WriteLine(valor.ToString("P", CultureInfo.CreateSpecificCulture("pt-BR"))); // porcentagem
-      Console.WriteLine(valor.ToString("E04", CultureInfo.CreateSpecificCulture("pt-BR")));
-      Console.WriteLine(valor.ToString("F", CultureInfo.CreateSpecificCulture("pt-BR")));
+      Console.WriteLine(Math.Round(valor)); // arredondar
+      Console.WriteLine(Math.Ceiling(valor)); // arredonda pra cima
+      Console.WriteLine(Math.Floor(valor)); // arredonda pra baixo
     }
   }
 }
