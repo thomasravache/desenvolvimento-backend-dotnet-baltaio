6480aaeec52db6e13a221f124ccf072daa03e0f5
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 21:58:17 -0300

    feat: Exceptions - Tratando erros

--- projects/Exceptions/Program.cs
+++ projects/Exceptions/Program.cs
@@ -5,13 +5,20 @@
     static void Main(string[] args)
     {
       var arr = new int[3];
-
+      
+      // Tratar os erros dos mais específicos para os mais genéricos
       try
       {
         for (var i = 0; i < 10; i++)
         {
           Console.WriteLine(arr[i]);
         }
+      }
+      catch(IndexOutOfRangeException ex)
+      {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine(ex.InnerException);
+        Console.WriteLine("Não encontrei o índice");
       }
       catch(Exception ex)
       {
