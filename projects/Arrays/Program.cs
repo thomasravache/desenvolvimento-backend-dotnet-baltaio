1d7cb5480c40009940559549412401844c0ac690
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 21:16:35 -0300

    feat: Arrays - Arrays

---
+++ projects/Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Arrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+
+      // array - não muito utilizado
+      var meuArray = new int[5]{ 20, 50, 10, 22, 98 }; // array de inteiros ja inicializado
+      meuArray[0] = 12;
+
+      var meuArray2 = new Teste[2]{ new Teste(), new Teste() };
+
+      Console.WriteLine(meuArray[0]);
+    }
+
+    struct Teste
+    {
+      public int Id { get; set; }
+    }
+  }
+}
