6d813dbdccbd5988cb58769df176273bc9fcc5a6
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 21:23:16 -0300

    feat: Arrays - Percorrendo um array

--- projects/Arrays/Program.cs
+++ projects/Arrays/Program.cs
@@ -10,14 +10,10 @@
       var meuArray = new int[5]{ 20, 50, 10, 22, 98 }; // array de inteiros ja inicializado
       meuArray[0] = 12;
 
-      var meuArray2 = new Teste[2]{ new Teste(), new Teste() };
-
-      Console.WriteLine(meuArray[0]);
-    }
-
-    struct Teste
-    {
-      public int Id { get; set; }
+      for (var index = 0; index < meuArray.Length; index++)
+      {
+        Console.WriteLine(meuArray[index]);
+      }
     }
   }
 }
