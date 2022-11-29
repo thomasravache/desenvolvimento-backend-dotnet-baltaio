0212ace962faf9ac1eff62ba9b66b8f186f00c73
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 21:47:35 -0300

    feat: Exceptions - Exceptions

---
+++ projects/Exceptions/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exceptions
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var arr = new int[3];
+
+      for (var i = 0; i < 10; i++)
+      {
+        // vai gerar Exception
+        Console.WriteLIne(arr[index]);
+      }
+    }
+  }
+}
