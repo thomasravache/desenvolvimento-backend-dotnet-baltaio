067e550f34ccead41e5713a608ce98a5c894f3eb
Thomas Ravache <thomasravache31@gmail.com>
2022-11-28 22:04:50 -0300

    feat: Exceptions - Disparando exceções

--- projects/Exceptions/Program.cs
+++ projects/Exceptions/Program.cs
@@ -5,14 +5,16 @@
     static void Main(string[] args)
     {
       var arr = new int[3];
-      
+
       // Tratar os erros dos mais específicos para os mais genéricos
       try
       {
-        for (var i = 0; i < 10; i++)
-        {
-          Console.WriteLine(arr[i]);
-        }
+        // for (var i = 0; i < 10; i++)
+        // {
+        //   Console.WriteLine(arr[i]);
+        // }
+
+        Salvar("");
       }
       catch(IndexOutOfRangeException ex)
       {
@@ -20,12 +22,24 @@
         Console.WriteLine(ex.InnerException);
         Console.WriteLine("Não encontrei o índice");
       }
+      catch(ArgumentNullException ex)
+      {
+        Console.WriteLine(ex.Message);
+        Console.WriteLine(ex.InnerException);
+        Console.WriteLine("Falha ao cadastrar texto");
+      }
       catch(Exception ex)
       {
         Console.WriteLine("Ops... Algo deu errado");
         Console.WriteLine(ex.Message);
         Console.WriteLine(ex.InnerException);
       }
+    }
+
+    private static void Salvar(string texto)
+    {
+      if (string.IsNullOrEmpty(texto))
+        throw new ArgumentNullException("Não pode ser nulo ou vazio");
     }
   }
 }
