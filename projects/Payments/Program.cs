14d05e1b79d9ea7ca6e31834be152164ece1ad00
Thomas Ravache <thomasravache31@gmail.com>
2022-12-02 10:51:21 -0300

    feat: Classe e Objeto

--- projects/Payments/Program.cs
+++ projects/Payments/Program.cs
@@ -6,7 +6,15 @@ namespace Payments
   {
     static void Main(string[] args)
     {
-      
+      var customer = new Customer();
+      customer.name = "teste";
+
+      Console.WriteLine("Hello World");
     }
+  }
+
+  class Customer
+  {
+    string name;
   }
 }
