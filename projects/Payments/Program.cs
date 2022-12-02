cd553077fd8964b2ea3c9d05c0e34f912dc637be
Thomas Ravache <thomasravache31@gmail.com>
2022-12-02 12:52:52 -0300

    feat: modificadores de acesso

--- projects/Payments/Program.cs
+++ projects/Payments/Program.cs
@@ -8,50 +8,32 @@ namespace Payments
     {
       var pagamentoBoleto = new PagamentoBoleto();
 
-      pagamentoBoleto.Pagar();
-      pagamentoBoleto.Vencimento = DateTime.Now;
-      var dataString = pagamentoBoleto.ToString();
+      pagamentoBoleto.Pagar(); // pode acessar pois método com classe base protected
+      pagamentoBoleto.Vencimento = DateTime.Now; // erro para acessar pois é privado
     }
   }
 
   // classe base
-  class Pagamento
+  public class Pagamento
   {
     // Propriedades
-    public DateTime Vencimento;
+    private DateTime Vencimento;
 
     // Métodos
 
     // virtual permite que o metodo seja sobrescrito
-    public virtual void Pagar()
+    protected virtual void Pagar()
     {
 
-    }
-
-    // método base do System
-    public override string ToString()
-    {
-      return Vencimento.ToString("dd/MM/yyyy");
     }
   }
 
-  class PagamentoBoleto : Pagamento
+  public class PagamentoBoleto : Pagamento
   {
-    public string NumeroBoleto;
-
-    public override void Pagar()
+    void Test()
     {
-      // Regra de pagamento do boleto
-    }
-  }
-
-  class PagamentoCartaoCredito : Pagamento
-  {
-    public string NumeroCartaoCredito;
-
-    public override void Pagar()
-    {
-      // Regra de pagamento do cartão de crédito
+      // acessar métodos do pai
+      base.Pagar();
     }
   }
 }
