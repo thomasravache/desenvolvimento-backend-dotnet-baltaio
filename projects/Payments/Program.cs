5db5fc591d0d3194c2b8a3044b4ceb6ea8bd4e7a
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-08 16:16:39 -0300

    feat: Interfaces

--- projects/Payments/Program.cs
+++ projects/Payments/Program.cs
@@ -6,9 +6,34 @@ namespace Payments
   {
     static void Main(string[] args)
     {
-      var pagamento = new Payment();
-      pagamento.PropriedadeCartaoCredito = 1;
-      pagamento.PropriedadePagamento = 2;
+
+    }
+
+    public class PagamentoCartaoCredito : IPagamento
+    {
+      public DateTime Vencimento { get; set; }
+
+      public void Pagar(double valor)
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public class Pagamento : IPagamento // classe que implementa o contrato IPagamento
+    {
+      public DateTime Vencimento { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+      public void Pagar(double valor)
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public interface IPagamento
+    {
+      DateTime Vencimento { get; set; } // precisa de uma propriedade Vencimento
+
+      void Pagar(double valor);
     }
   }
 }
