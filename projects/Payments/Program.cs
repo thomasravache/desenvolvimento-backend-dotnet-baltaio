f8d659c50abf0fbfc8a3a81c7ad4dfc61c3a0f00
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-12 17:02:25 -0300

    feat: Events

--- projects/Payments/Program.cs
+++ projects/Payments/Program.cs
@@ -4,21 +4,56 @@ namespace Payments
 {
   class Program
   {
-    static void RealizarPagamento(double valor)
-    {
-      Console.WriteLine($"Pago o valor de {valor}");
-    }
-
     static void Main(string[] args)
     {
-      // Delegates => Anonymous Methods
-      var pagar = new Pagamento.Pagar(RealizarPagamento); // função que chama uma outra função
-      pagar(25);
+      var room = new Room(3);
+      room.RoomSoldOutEvent += OnRoomSoldOut;
+
+      room.ReserveSeat();
+      room.ReserveSeat();
+      room.ReserveSeat();
+      room.ReserveSeat();
+    }
+
+    static void OnRoomSoldOut(object sender, EventArgs e)
+    {
+      Console.WriteLine("Sala lotada!");
     }
   }
 
-  public class Pagamento
+  public class Room
   {
-    public delegate void Pagar(double valor);
+    public Room(int seats)
+    {
+      Seats = seats;
+      seatsInUse = 0;
+    }
+
+    private int seatsInUse = 0;
+    public int Seats { get; set; }
+
+    public void ReserveSeat()
+    {
+      seatsInUse++;
+
+      if (seatsInUse >= Seats)
+      {
+        OnRoomSoldOut(EventArgs.Empty);
+      }
+      else
+      {
+        Console.WriteLine("Assento reservado.");
+      }
+    }
+
+    // assinatura do  evento em si
+    public event EventHandler RoomSoldOutEvent;
+
+    // manipulador do evento
+    protected virtual void OnRoomSoldOut(EventArgs e)
+    {
+      EventHandler handler = RoomSoldOutEvent;
+      handler?.Invoke(this, e);
+    }
   }
 }
