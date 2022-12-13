e19f2e400ac66f3551da32b8b5842e0ff1988df4
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-13 15:34:09 -0300

    feat: Listas - Parte 2

--- projects/Payments/Program.cs
+++ projects/Payments/Program.cs
@@ -7,13 +7,43 @@ namespace Payments
     static void Main(string[] args)
     {
       IList<Payment> payments = new List<Payment>();
-      payments.Add(new Payment());
-      payments.Remove(new Payment());
+
+      payments.Add(new Payment(1));
+      payments.Add(new Payment(2));
+      payments.Add(new Payment(3));
+      payments.Add(new Payment(4));
+      payments.Add(new Payment(5));
+
+      payments.Remove(new Payment(1));
+
+      foreach (var item in payments)
+      {
+        Console.WriteLine(item.Id); ;
+      }
+
+      var paidPayments = new List<Payment>();
+
+      paidPayments.AddRange(payments);
+
+      var payment = payments.First(x => x.Id == 3);
+      Console.WriteLine(payment.Id);
+
+      payments.Remove(payment);
+
+      payments.Clear(); // limpar a lista
+
+      var exists = payments.Any(x => x.Id == 3);
+      Console.WriteLine(exists);
     }
   }
 
   public class Payment
   {
+    public int Id { get; set; }
 
+    public Payment(int id)
+    {
+      Id = id;
+    }
   }
 }
