b725427f57400e98ee053b1da8b21c47a4c05603
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-13 18:38:33 -0300

    feat: Trabalhando com listas

--- projects/Balta/ContentContext/Course.cs
+++ projects/Balta/ContentContext/Course.cs
@@ -2,6 +2,30 @@ namespace Balta.ContentContext
 {
   public class Course : Content
   {
+    public Course()
+    {
+      Modules = new List<Module>(); // sempre inicializar as listas
+    }
 
+    public string Tag { get; set; }
+    public IList<Module> Modules { get; set; }
+  }
+
+  public class Module
+  {
+    public Module()
+    {
+      Lectures = new List<Lecture>();
+    }
+
+    public int Order { get; set; }
+    public string Title { get; set; }
+    public IList<Lecture> Lectures { get; set; }
+  }
+
+  public class Lecture
+  {
+    public int Order { get; set; }
+    public string Title { get; set; }
   }
 }
