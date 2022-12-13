ff628903728e9887f43794b312d9c461250671f6
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-13 14:56:00 -0300

    feat: Listas - Parte 1

--- projects/Payments/Program.cs
+++ projects/Payments/Program.cs
@@ -6,55 +6,13 @@ namespace Payments
   {
     static void Main(string[] args)
     {
-      var person = new Person();
-      var payment = new Payment();
-      var subscription = new Subscription();
-
-      var context = new DataContext<Person, Payment, Subscription>();
-
-      context.Save(person);
-      context.Save(payment);
-      context.Save(subscription);
+      IList<Payment> payments = new List<Payment>();
+      payments.Add(new Payment());
+      payments.Remove(new Payment());
     }
-  }
-
-  public class DataContext<T, U, V>
-    where T : IPerson
-    where U : Payment
-    where V : Subscription
-  {
-    public void Save(T entity)
-    {
-
-    }
-
-    public void Save(U entity)
-    {
-
-    }
-
-    public void Save(V entity)
-    {
-
-    }
-  }
-
-  public interface IPerson
-  {
-
-  }
-
-  public class Person : IPerson
-  {
-
   }
 
   public class Payment
-  {
-
-  }
-
-  public class Subscription
   {
 
   }
