ca5d77dbc04d78894e1714c35e64fc00bf493c8e
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-13 20:11:10 -0300

    feat: Interagindo com listas

--- projects/Balta/Program.cs
+++ projects/Balta/Program.cs
@@ -17,6 +17,38 @@ namespace Balta
         Console.WriteLine(article.Title);
         Console.WriteLine(article.Url);
       }
+
+      var courses = new List<Course>();
+
+      var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+      var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+      var courseAspNet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+
+      courses.Add(courseOOP);
+      courses.Add(courseCsharp);
+      courses.Add(courseAspNet);
+
+      var careers = new List<Career>();
+
+      var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+      var careerItem = new CareerItem(1, "Aprenda .NET", "", null);
+      var careerItem2 = new CareerItem(2, "Comece por aqui", "", null);
+      var careerItem3 = new CareerItem(3, "Aprenda OOP", "", null);
+
+      careerDotnet.Items.Add(careerItem2);
+      careerDotnet.Items.Add(careerItem3);
+      careerDotnet.Items.Add(careerItem);
+
+      careers.Add(career);
+
+      foreach (var career in careers)
+      {
+        Console.WriteLine(career.Title);
+        foreach (var item in career.Items.OrderBy(x => x.Ordem))
+        {
+          Console.WriteLine($"{item.Ordem}. {item.Title}");
+        }
+      }
     }
   }
 }
