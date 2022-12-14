c9f9b229728741b55bcbbdce148e1b85057fc6ea
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-12-14 10:56:41 -0300

    feat: Validando as regras de negócio

--- projects/Balta/SubscriptionContext/Student.cs
+++ projects/Balta/SubscriptionContext/Student.cs
@@ -7,5 +7,9 @@ namespace Balta.SubscriptionContext
     public string Name { get; set; }
     public string Email { get; set; }
     public User User { get; set; }
+
+    public IList<Subscription> Subscriptions { get; set; }
+
+    public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
   }
 }
