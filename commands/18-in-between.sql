573064a0d6736b4f253fa6c7f1265ab9e9e362cb
thomas.santos <thomas.santos@conveste.com.br>
2022-12-15 14:36:54 -0300

    feat: In, Between

---
+++ commands/18-in-between.sql
@@ -0,0 +1,9 @@
+SELECT TOP 100
+  *
+FROM
+  [Curso]
+WHERE
+  [Id] BETWEEN 1 AND 3
+
+  -- IN = se o valor está presente, possível utilizar um select nele também
+  -- BETWEEN = valor está entre um e outro
