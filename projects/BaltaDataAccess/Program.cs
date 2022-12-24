3ac1a235ab83c8cdb31a77f2130d5011f6d0651c
thomas.santos <thomas.santos@conveste.com.br>
2022-12-24 11:08:00 -0300

    feat: Connection String

---
+++ projects/BaltaDataAccess/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BaltaDataAccess
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      const string connectionString = "Server=127.0.0.1,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+    }
+  }
+}
