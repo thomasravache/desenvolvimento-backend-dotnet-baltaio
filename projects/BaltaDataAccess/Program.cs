af4e1527c43ed79223a9ac318396f1f5d4884982
thomas.santos <thomas.santos@conveste.com.br>
2022-12-24 11:21:04 -0300

    feat: Microsoft.Data.SqlClient

--- projects/BaltaDataAccess/Program.cs
+++ projects/BaltaDataAccess/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace BaltaDataAccess
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
 {
   class Program
   {
     static void Main(string[] args)
     {
       const string connectionString = "Server=127.0.0.1,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
     }
   }
 }
