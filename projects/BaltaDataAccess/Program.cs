3b95cf1aa77072c3cec24bf5b94c54e413cb0c28
thomas.santos <thomas.santos@conveste.com.br>
2022-12-24 11:41:35 -0300

    feat SqlCommand e SqlDataReader

--- projects/BaltaDataAccess/Program.cs
+++ projects/BaltaDataAccess/Program.cs
@@ -11,7 +11,21 @@ namespace BaltaDataAccess
 
       using (var connection = new SqlConnection(connectionString)) // objeto de conexão
       {
+        connection.Open();
+        Console.WriteLine("Conectado");
 
+        using (var command = new SqlCommand())
+        {
+          command.Connection = connection;
+          command.CommandType = System.Data.CommandType.Text;
+          command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+          var reader = command.ExecuteReader(); // ou  ExecutaNonQuery para não leitura (inserção etc...)
+          while (reader.Read())
+          {
+            Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+          }
+        }
       }
 
       // BOA PRÁTICA: conectar no banco, fazer o necessário e fechar a conexão
