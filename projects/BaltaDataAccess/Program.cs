d5873c12ec1bc426ce65d31cae71dfaef8f7e8d5
thomas.santos <thomas.santos@conveste.com.br>
2022-12-25 23:05:50 -0300

    feat: UPDATE

--- projects/BaltaDataAccess/Program.cs
+++ projects/BaltaDataAccess/Program.cs
@@ -11,6 +11,26 @@ namespace BaltaDataAccess
     {
       const string connectionString = "Server=127.0.0.1,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
+      using (var connection = new SqlConnection(connectionString)) // objeto de conexão
+      {
+        ListCategories(connection);
+        // CreateCategory(connection);
+        UpdateCategory(connection);
+      }
+    }
+
+    static void ListCategories(SqlConnection connection)
+    {
+      var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+      foreach (var item in categories)
+      {
+        Console.WriteLine($"{item.Id} - {item.Title}");
+      }
+    }
+
+    static void CreateCategory(SqlConnection connection)
+    {
       var category = new Category();
       category.Id = Guid.NewGuid();
       category.Title = "Amazon AWS";
@@ -31,29 +51,32 @@ namespace BaltaDataAccess
           @Description,
           @Featured)";
 
-      using (var connection = new SqlConnection(connectionString)) // objeto de conexão
+      // Utilizar o .Execute para Insert, Update, Delete
+      var rows = connection.Execute(insertSql, new
       {
-        // Utilizar o .Execute para Insert, Update, Delete
-        var rows = connection.Execute(insertSql, new
-        {
-          category.Id,
-          category.Title,
-          category.Url,
-          category.Summary,
-          category.Order,
-          category.Description,
-          category.Featured
-        }); // passando valores por parâmetro pra evitar SQL Injection
+        category.Id,
+        category.Title,
+        category.Url,
+        category.Summary,
+        category.Order,
+        category.Description,
+        category.Featured
+      }); // passando valores por parâmetro pra evitar SQL Injection
 
-        Console.WriteLine($"{rows} linhas inseridas");
+      Console.WriteLine($"{rows} linhas inseridas");
+    }
 
-        var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+    static void UpdateCategory(SqlConnection connection)
+    {
+      var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
 
-        foreach (var item in categories)
-        {
-          Console.WriteLine($"{item.Id} - {item.Title}");
-        }
-      }
+      var rows = connection.Execute(updateQuery, new
+      {
+        id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+        title = "Frontend 2021"
+      });
+
+      Console.WriteLine($"{rows} registros atualizados");
     }
   }
 }
