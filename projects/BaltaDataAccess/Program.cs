b006ee0585ff54f24f1374b30acf9565bfffa402
thomas.santos <thomas.santos@conveste.com.br>
2022-12-25 22:42:04 -0300

    feat: SQL Injection

--- projects/BaltaDataAccess/Program.cs
+++ projects/BaltaDataAccess/Program.cs
@@ -12,7 +12,24 @@ namespace BaltaDataAccess
       const string connectionString = "Server=127.0.0.1,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
 
       var category = new Category();
-      var insertSql = "INSERT INTO [Category] VALUES(id, title, url, summary, order, description, featured)";
+      category.Id = Guid.NewGuid();
+      category.Title = "Amazon AWS";
+      category.Url = "amazon";
+      category.Description = "Categoria destinada a serviços do AWS";
+      category.Order = 8;
+      category.Summary = "AWS Cloud";
+      category.Featured = false;
+
+      var insertSql = @"INSERT INTO
+          [Category]
+        VALUES(
+          id,
+          title,
+          url,
+          summary,
+          order,
+          description,
+          featured)";
 
       using (var connection = new SqlConnection(connectionString)) // objeto de conexão
       {
