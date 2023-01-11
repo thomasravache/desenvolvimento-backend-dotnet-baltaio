7c4690fcf05d9e1f55df9efa5bcc5ea6a4a259d6
thomas.santos <thomas.santos@conveste.com.br>
2023-01-11 19:25:04 -0300

    feat: Criando o projeto

---
+++ projects/Blog/Program.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+  public class Program
+  {
+    private const string CONNECTION_STRING = @"Server=127.0.0.1,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+
+    static void Main(string[] args)
+    {
+
+    }
+
+    public static void ReadUsers()
+    {
+      using (var connection = new SqlConnection(CONNECTION_STRING))
+      {
+        var users = connection.GetAll<User>();
+
+        foreach (var user in users)
+        {
+          Console.WriteLine(user.Name);
+        }
+      }
+    }
+  }
+}
