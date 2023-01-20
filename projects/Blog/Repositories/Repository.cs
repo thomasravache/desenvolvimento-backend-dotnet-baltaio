fe4b02f376eff5f4c2f25101c4d3b66580806036
Thomas Ravache <thomas.santos@conveste.com.br>
2023-01-20 11:20:45 -0300

    feat: Repositório Genérico

---
+++ projects/Blog/Repositories/Repository.cs
@@ -0,0 +1,17 @@
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Repositories
+{
+    public class Repository<T>
+        where T : class
+    {
+        private readonly SqlConnection _connection;
+
+        public Repository(SqlConnection connection)
+            => _connection = connection;
+
+        public IEnumerable<T> Get()
+            => _connection.GetAll<T>();
+    }
+}
