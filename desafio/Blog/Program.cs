76cfe743e8a6a8d0bee1669a64d862575c62a7bb
Thomas Ravache <thomasravache31@gmail.com>
2023-01-29 22:33:49 -0300

    feat: Compartilhando a conexão

--- desafio/Blog/Program.cs
+++ desafio/Blog/Program.cs
@@ -6,65 +6,66 @@ using Microsoft.Data.SqlClient;
 
 namespace Blog
 {
-    public class Program
+  public class Program
+  {
+    private const string CONNECTION_STRING = @"Server=127.0.0.1,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+
+    static void Main(string[] args)
     {
-        private const string CONNECTION_STRING = @"Server=127.0.0.1,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+      Database.Connection = new SqlConnection(CONNECTION_STRING);
 
-        static void Main(string[] args)
-        {
-            var connection = new SqlConnection(CONNECTION_STRING);
-            connection.Open();
+      Database.Connection.Open();
 
-            Load();
+      Load();
 
-            Console.ReadKey();
-            connection.Close();
-        }
-
-        private static void Load()
-        {
-            Console.Clear();
-            Console.WriteLine("Meu Blog");
-            Console.WriteLine("----------------");
-            Console.WriteLine("O que deseja fazer?");
-            Console.WriteLine();
-            Console.WriteLine("1 - Gestão de usuário");
-            Console.WriteLine("2 - Gestão de perfil");
-            Console.WriteLine("3 - Gestão de categoria");
-            Console.WriteLine("4 - Gestão de tag");
-            Console.WriteLine("5 - Vincular perfil/usuário");
-            Console.WriteLine("6 - Vincular post/tag");
-            Console.WriteLine("7 - Relatórios");
-            Console.WriteLine();
-            Console.WriteLine();
-
-            var option = short.Parse(Console.ReadLine()!);
-
-            switch (option)
-            {
-                // case 1:
-                //     MenuUserScreen.Load();
-                //     break;
-                // case 2:
-                //     MenuRoleScreen.Load();
-                //     break;
-                // case 3:
-                //     MenuCategoryScreen.Load();
-                //     break;
-                case 4:
-                    MenuTagScreen.Load();
-                    break;
-                // case 5:
-                //     MenuProfileUserScreen.Load();
-                //     break;
-                // case 6:
-                //     MenuPostTagScreen.Load();
-                //     break;
-                // case 7:
-                //     MenuReportsScreen.Load();
-                //     break;
-                default: Load(); break;
-            }
-        }
+      Console.ReadKey();
+      Database.Connection.Close();
     }
+
+    private static void Load()
+    {
+      Console.Clear();
+      Console.WriteLine("Meu Blog");
+      Console.WriteLine("----------------");
+      Console.WriteLine("O que deseja fazer?");
+      Console.WriteLine();
+      Console.WriteLine("1 - Gestão de usuário");
+      Console.WriteLine("2 - Gestão de perfil");
+      Console.WriteLine("3 - Gestão de categoria");
+      Console.WriteLine("4 - Gestão de tag");
+      Console.WriteLine("5 - Vincular perfil/usuário");
+      Console.WriteLine("6 - Vincular post/tag");
+      Console.WriteLine("7 - Relatórios");
+      Console.WriteLine();
+      Console.WriteLine();
+
+      var option = short.Parse(Console.ReadLine()!);
+
+      switch (option)
+      {
+        // case 1:
+        //     MenuUserScreen.Load();
+        //     break;
+        // case 2:
+        //     MenuRoleScreen.Load();
+        //     break;
+        // case 3:
+        //     MenuCategoryScreen.Load();
+        //     break;
+        case 4:
+          MenuTagScreen.Load();
+          break;
+        // case 5:
+        //     MenuProfileUserScreen.Load();
+        //     break;
+        // case 6:
+        //     MenuPostTagScreen.Load();
+        //     break;
+        // case 7:
+        //     MenuReportsScreen.Load();
+        //     break;
+        default: Load(); break;
+      }
+    }
+  }
 }
--- desafio/Blog/Screens/TagScreens/ListTagScreen.cs
+++ desafio/Blog/Screens/TagScreens/ListTagScreen.cs
@@ -1,10 +1,28 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.TagScreens
 {
-    public static class ListTagScreen
+  public static class ListTagScreen
+  {
+    public static void Load()
     {
-        public static void Load()
-        {
-
-        }
+      Console.WriteLine("Lista de tags");
+      Console.WriteLine("----------------");
+      List();
+      Console.ReadKey();
     }
+
+    private static void List()
+    {
+      var repository = new Repository<Tag>(Database.Connection);
+
+      var tags = repository.Get();
+
+      foreach (var item in tags)
+      {
+        Console.WriteLine($"{item.Id} - {item.Name} ({item.Slug})");
+      }
+    }
+  }
 }
