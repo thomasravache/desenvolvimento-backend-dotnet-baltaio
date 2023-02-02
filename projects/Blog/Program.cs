dc40d7cbf0542c5a358356eaa9ee2c23e2e85fef
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-02 19:45:03 -0300

    feat: First, Single

--- projects/Blog/Program.cs
+++ projects/Blog/Program.cs
@@ -15,16 +15,16 @@ namespace Blog
                 // context.Tags.Add(tag);
                 // context.SaveChanges();
 
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
 
-                if (tag == null)
-                    return;
+                // if (tag == null)
+                //     return;
 
-                tag.Name = "Ponto NET";
-                tag.Slug = "dotnetzada";
+                // tag.Name = "Ponto NET";
+                // tag.Slug = "dotnetzada";
 
-                context.Update(tag);
-                context.SaveChanges();
+                // context.Update(tag);
+                // context.SaveChanges();
 
                 // var tag = context.Tags.FirstOrDefault(x => x.Id == 1003);
 
@@ -49,6 +49,14 @@ namespace Blog
                 // pontos importantes
                 // onde coloca o ToList()
                 // utilizar AsNoTracking
+
+                var tag = context.Tags.AsNoTracking().FirstOrDefault(x => x.Id == 2);
+
+                // Da mesma form que toList executa a query o firstOrDefault também,
+                // todos os métodos de lista executam a query
+
+                // FirstOrDefault => pega o primeiro mesmo se trazer mais de um
+                // Single / SingleOrDefault => se tiver mais de um resultado dá erro
             }
         }
     }
