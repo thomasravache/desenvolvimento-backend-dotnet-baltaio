e496c016e88c43fa6ec6cbb372223379810e4d73
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-01 21:17:23 -0300

    feat: DbSet

---
+++ projects/Blog/Data/BlogDataContext.cs
@@ -0,0 +1,10 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
--- projects/Blog/Program.cs
+++ projects/Blog/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Blog.Data;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new BlogDataContext();
+
+            // ctx.Categories;
+        }
+    }
+}
