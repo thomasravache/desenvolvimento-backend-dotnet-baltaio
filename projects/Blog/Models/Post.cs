cdc2039418ab76a276ba822fbe8e137846d738bd
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-03 17:35:20 -0300

    feat: Navigation Properties

--- projects/Blog/Models/Post.cs
+++ projects/Blog/Models/Post.cs
@@ -1,15 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("Post")]
     public class Post
     {
+        [Key] // Deixa explícito que o Id é uma primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Id é gerado pelo banco com Identity, então explicitamos aqui
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public int AuthorId { get; set; }
+
         public string Title { get; set; } = null!;
         public string Summary { get; set; } = null!;
         public string Body { get; set; } = null!;
         public string Slug { get; set; } = null!;
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
+        /*
+            o valor passado para o Annotation ForeignKey vai separar pelo CamelCase
+            procurando nesse caso a referência na tabela 'Cateogory' na propriedade 'Id'
+            (CategoryId = Cateogory | Id)
+        */
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        // a propriedade do tipo Cateogory é uma Navigation Property, o banco vai fazer o join pra gente
+        public Category Category { get; set; } = null!;
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+        public User Author { get; set; } = null!;
     }
 }
--- projects/Blog/Models/User.cs
+++ projects/Blog/Models/User.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key] // Deixa explícito que o Id é uma primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Id é gerado pelo banco com Identity, então explicitamos aqui
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Email { get; set; } = null!;
