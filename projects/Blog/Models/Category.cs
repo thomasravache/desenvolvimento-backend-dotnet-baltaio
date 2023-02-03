50b1daef70a428deabab6fec9bab0090e2e405a8
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-03 17:13:55 -0300

    feat: Table, Key e Identity

--- projects/Blog/Models/Category.cs
+++ projects/Blog/Models/Category.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("Category")] // Se não informar o EF assume que é o mesmo nome informado no referente DbSet
     public class Category
     {
+        [Key] // Deixa explícito que o Id é uma primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Id é gerado pelo banco com Identity, então explicitamos aqui
         public int Id { get; set; }
+
+
         public string Name { get; set; } = null!;
         public string Slug { get; set; } = null!;
     }
