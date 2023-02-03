bc36afa30bd08da35b3bbef01176c755e4d7813a
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-03 17:20:30 -0300

    feat: Required, MaxLength e Column

--- projects/Blog/Models/Category.cs
+++ projects/Blog/Models/Category.cs
@@ -10,8 +10,16 @@ namespace Blog.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Id é gerado pelo banco com Identity, então explicitamos aqui
         public int Id { get; set; }
 
-
+        [Required] // Informa que esse campo no banco é NOT NULL
+        [MinLength(3)] // É possível validar a nível de código isso aqui, mesmo não existindo minLength no banco
+        [MaxLength(80)]
+        [Column("Name", TypeName = "NVARCHAR")]
         public string Name { get; set; } = null!;
+
+        [Required] // Informa que esse campo no banco é NOT NULL
+        [MinLength(3)] // É possível validar a nível de código isso aqui, mesmo não existindo minLength no banco
+        [MaxLength(80)]
+        [Column("Slug", TypeName = "VARCHAR")]
         public string Slug { get; set; } = null!;
     }
 }
