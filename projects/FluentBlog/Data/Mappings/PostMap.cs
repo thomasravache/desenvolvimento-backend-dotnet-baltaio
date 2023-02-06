7daa471bee603ef5d9546bf7b67e3eb31b72c023
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-06 14:13:11 -0300

    feat: Default Values

---
+++ projects/FluentBlog/Data/Mappings/PostMap.cs
@@ -0,0 +1,31 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class PostMap : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.ToTable("Post");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.LastUpdateDate)
+                .IsRequired()
+                .HasColumnName("LastUpdateDate")
+                .HasColumnType("SMALLDATETIME")
+                .HasMaxLength(60)
+                .HasDefaultValueSql("GETDATE()");
+            // .HasDefaultValue(DateTime.Now.ToUniversalTime());
+
+            builder.HasIndex(x => x.Slug, "IX_Post_Slug")
+                .IsUnique();
+        }
+    }
+}
