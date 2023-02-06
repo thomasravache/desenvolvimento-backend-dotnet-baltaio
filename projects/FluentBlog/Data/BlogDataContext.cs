c2ef6d20184ccf30d93a018116e798d083a83859
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-06 17:00:55 -0300

    feat: utilizando os mapeamentos criados

--- projects/FluentBlog/Data/BlogDataContext.cs
+++ projects/FluentBlog/Data/BlogDataContext.cs
@@ -1,3 +1,4 @@
+using Blog.Data.Mappings;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,10 +6,21 @@ namespace Blog.Data
 {
     public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer("Server=127.0.0.1,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$");
+            options.UseSqlServer("Server=127.0.0.1,1433;Database=BlogFluentMap;User ID=sa;Password=1q2w3e4r@#$");
             options.LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
         }
     }
 }
--- projects/FluentBlog/Data/Mappings/UserMap.cs
+++ projects/FluentBlog/Data/Mappings/UserMap.cs
@@ -37,6 +37,24 @@ namespace Blog.Data.Mappings
 
             builder.HasIndex(x => x.Slug, "IX_User_Slug")
                 .IsUnique();
+
+            builder.HasMany(x => x.Roles)
+                .WithMany(x => x.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "UserRole",
+                    role => role
+                        .HasOne<Role>()
+                        .WithMany()
+                        .HasForeignKey("RoleId")
+                        .HasConstraintName("FK_UserRole_RoleId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    user => user
+                        .HasOne<User>()
+                        .WithMany()
+                        .HasForeignKey("UserId")
+                        .HasConstraintName("FK_UserRole_UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
