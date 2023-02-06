34826b124aaf1d54f5cdbf8351a28edc8d47f591
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-06 16:36:33 -0300

    feat: Relacionamento muitos para muitos

--- projects/FluentBlog/Data/Mappings/PostMap.cs
+++ projects/FluentBlog/Data/Mappings/PostMap.cs
@@ -39,6 +39,22 @@ namespace Blog.Data.Mappings
                 .HasForeignKey(x => x.CategoryId)
                 .HasConstraintName("FK_Post_Category")
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>( // gera uma entidade virtual
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
