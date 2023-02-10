f1be9fb3296b7c62086e35f7daebc872769c54bb
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-10 11:13:11 -0300

    feat: Serialização JSON

--- projects/MinhaApi/Program.cs
+++ projects/MinhaApi/Program.cs
@@ -10,4 +10,19 @@ app.MapGet("/{nome}", (string nome) =>
     return Results.Ok($"Hello {nome}");
 });
 
+app.MapPost("/", (User user) =>
+{
+    /*
+        JSON pra objeto CSharp = serialização
+        objeto CSharp pra JSON = deserialização
+    */
+    return Results.Ok(user);
+});
+
 app.Run(); // rodar a aplicação
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = null!;
+}
