9e0d29c29356244568dc38446252994036d2ccb0
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-10 10:44:06 -0300

    feat: Mapeando uma requisição

--- projects/MinhaApi/Program.cs
+++ projects/MinhaApi/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args); // Cria a aplicação web
 var app = builder.Build(); // construir a aplicação
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/xablau", () => "xablau!");
 
 app.Run(); // rodar a aplicação
