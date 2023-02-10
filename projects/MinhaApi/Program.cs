18105d144129995413ad039a5b462f9298904dde
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-10 10:49:56 -0300

    feat: Funções anônimas

--- projects/MinhaApi/Program.cs
+++ projects/MinhaApi/Program.cs
@@ -1,7 +1,16 @@
 var builder = WebApplication.CreateBuilder(args); // Cria a aplicação web
 var app = builder.Build(); // construir a aplicação
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return "Hello World!";
+});
+// app.MapGet("/", Teste);
 app.MapGet("/xablau", () => "xablau!");
 
 app.Run(); // rodar a aplicação
+
+// string Teste()
+// {
+//     return "Hello World!";
+// }
