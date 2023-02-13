274c5528111e1176c50ae4b5d2549f353c564d82
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-13 08:37:08 -0300

    feat: Criando um registro

--- projects/Todo/Controllers/HomeController.cs
+++ projects/Todo/Controllers/HomeController.cs
@@ -5,16 +5,27 @@ using Todo.Models;
 namespace Todo.Controllers
 {
     [ApiController]
-    [Route("[controller]")] // prefixo de rota (mesmo que escrever "Home")
+    // [Route("[controller]")] // prefixo de rota (mesmo que escrever "Home")
     public class HomeController : ControllerBase
     {
-        [HttpGet] // ou [HttpGet("/rota")]
-        [Route("[action]")] // mesmo que escrever "Get"
+        [HttpGet("/")] // ou [HttpGet("/rota")]
         public List<TodoModel> Get(
             [FromServices] AppDbContext context // Adquirir o contexto por injeção de dependências
         )
         {
             return context.Todos.ToList();
+        }
+
+        [HttpPost("/")] // ou [HttpGet("/rota")]
+        public TodoModel Post(
+            [FromServices] AppDbContext context, // Adquirir o contexto por injeção de dependências
+            [FromBody] TodoModel todo
+        )
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return todo;
         }
     }
 }
