d677943cca0407b9dcfd3035f46ae3701da67c02
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-13 08:56:06 -0300

    feat: Atualizando e excluindo um registro

--- projects/Todo/Controllers/HomeController.cs
+++ projects/Todo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Todo.Data;
 using Todo.Models;
 
@@ -8,12 +9,21 @@ namespace Todo.Controllers
     // [Route("[controller]")] // prefixo de rota (mesmo que escrever "Home")
     public class HomeController : ControllerBase
     {
-        [HttpGet("/")] // ou [HttpGet("/rota")]
+        [HttpGet("/")]
         public List<TodoModel> Get(
             [FromServices] AppDbContext context // Adquirir o contexto por injeção de dependências
         )
         {
-            return context.Todos.ToList();
+            return context.Todos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+            [FromServices] AppDbContext context, // Adquirir o contexto por injeção de dependências
+            [FromRoute] int id
+        )
+        {
+            return context.Todos.AsNoTracking().FirstOrDefault(x => x.Id == id);
         }
 
         [HttpPost("/")] // ou [HttpGet("/rota")]
@@ -26,6 +36,42 @@ namespace Todo.Controllers
             context.SaveChanges();
 
             return todo;
+        }
+
+        [HttpPut("/{id:int}")] // ou [HttpGet("/rota")]
+        public TodoModel Put(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context // Adquirir o contexto por injeção de dependências
+        )
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+
+            return model;
+        }
+
+        [HttpDelete("/{id:int}")] // ou [HttpGet("/rota")]
+        public TodoModel Delete(
+            [FromRoute] int id,
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context // Adquirir o contexto por injeção de dependências
+        )
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+
+            return model;
         }
     }
 }
