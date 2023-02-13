c9d744e76a7f832bb5c82efb554cdfae8b81797a
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-13 08:14:17 -0300

    feat: Adicionando suporte a Controllers

--- projects/Todo/Program.cs
+++ projects/Todo/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Diz pro ASP.NET que a API está dividida em controllers
+// Procura tudo que tem o sufixo Controller e herda de um ControllerBase
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-// app.MapGet("/", () => "Hello World!");
+app.MapControllers(); // Faz a mesma função do MapGet, MapPost etc...
 
 app.Run();
