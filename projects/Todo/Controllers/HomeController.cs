e7b60e84e37e11e236f5059d1c1885408a980de1
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-13 08:08:07 -0300

    feat: Rotas e Controllers

--- projects/Todo/Controllers/HomeController.cs
+++ projects/Todo/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 namespace Todo.Controllers
 {
     [ApiController]
+    [Route("[controller]")] // prefixo de rota (mesmo que escrever "Home")
     public class HomeController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet] // ou [HttpGet("/rota")]
+        [Route("[action]")] // mesmo que escrever "Get"
         public string Get()
         {
             return "Hello World!";
