d980e8dfbee95aa864592b23017f193a60c52e80
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-14 08:08:05 -0300

    feat: CRUD de Categorias

--- projects/Blog/Controllers/CategoryController.cs
+++ projects/Blog/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,73 @@ namespace Blog.Controllers
             var categories = await context.Categories.AsNoTracking().ToListAsync();
 
             return Ok(categories);
+        }
+
+        [HttpGet("v1/categories/{id:int}")] // dá pra tipar os parâmetros de rota
+        public async Task<IActionResult> GetByIdAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context
+        )
+        {
+            var category = await context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost("v1/categories")] // dá pra tipar os parâmetros de rota
+        public async Task<IActionResult> PostAsync(
+            [FromBody] Category model,
+            [FromServices] BlogDataContext context
+        )
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            return Created($"v1/categories/{model.Id}", model);
+        }
+
+        [HttpPut("v1/categories/{id:int}")] // dá pra tipar os parâmetros de rota
+        public async Task<IActionResult> PutAsync(
+            [FromRoute] int id,
+            [FromBody] Category model,
+            [FromServices] BlogDataContext context
+        )
+        {
+            var category = await context.Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+        [HttpPut("v1/categories/{id:int}")] // dá pra tipar os parâmetros de rota
+        public async Task<IActionResult> DeleteAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context
+        )
+        {
+            var category = await context.Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
+
+            return Ok(category);
         }
     }
 }
