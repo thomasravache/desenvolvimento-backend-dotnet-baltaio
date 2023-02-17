cd8d594bc34fca44c02c010537b7d4a7ea6ab7b6
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-17 17:15:38 -0300

    feat: Gerando o token

--- projects/Blog/Services/TokenService.cs
+++ projects/Blog/Services/TokenService.cs
@@ -11,7 +11,14 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler(); // Cria instancia do manipulador de token (espera a chave convertida  em um array de bytes)
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey); // converte a chave pra um array de bytes
-        var tokenDescriptor = new SecurityTokenDescriptor();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddHours(8), // tempo de expiração do token
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key), // usada para desencriptar e encriptar
+                SecurityAlgorithms.HmacSha256Signature // Algoritmo utilizado
+            ) // forma como vai gerar o token e encriptá-lo / desencriptá-lo
+        };
         var token = tokenHandler.CreateToken(tokenDescriptor); // cria o token baseado nas opções do tokenDescriptor
 
         return tokenHandler.WriteToken(token); // retornará uma string baseada no token
