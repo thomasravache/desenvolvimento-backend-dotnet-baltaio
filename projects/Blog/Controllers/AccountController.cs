eb9dfd2d4a0aca27f742747a8a5352edf4a94677
Thomas Ravache <thomas.santos@conveste.com.br>
2023-02-22 08:16:34 -0300

    feat: Injeção de Dependência

---
+++ projects/Blog/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    /*
+        Criar uma dependência do que sua classe precisa pra trabalhar
+
+        Até a linha 20 seria a mesma coisa que utilizar um FromServices no método desejado
+    */
+    private readonly TokenService _tokenService;
+
+    public AccountController(TokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    [HttpPost("v1/login")]
+    public IActionResult Login()
+    {
+        var tokenService = new TokenService();
+        var token = tokenService.GenerateToken(null);
+
+        return Ok(token);
+    }
+}
