7b9e2344efe4ac0cfcbb5d13cc4460c33a9dc023
Thomas Ravache <thomas.santos@conveste.com.br>
2023-03-16 22:38:00 -0300

    feat: Filtrando dados

--- projects/Blog/Controllers/PostController.cs
+++ projects/Blog/Controllers/PostController.cs
@@ -59,4 +59,80 @@ public class PostController : ControllerBase
             return StatusCode(StatusCodes.Status500InternalServerError, new ResultViewModel<List<Post>>("05X04 - Falha interna no servidor"));
         }
     }
+
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> GetAsync(
+        [FromServices] BlogDataContext context,
+        [FromRoute] int id
+    )
+    {
+        try
+        {
+            var postsCount = await context.Posts.AsNoTracking().CountAsync();
+
+            var post = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                    .ThenInclude(x => x.Roles)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (post == null)
+                return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+
+            return Ok(new ResultViewModel<Post>(post));
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResultViewModel<Post>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/category/{categorySlug}")]
+    public async Task<IActionResult> GetByCategoryAsync(
+        [FromRoute] string categorySlug,
+        [FromServices] BlogDataContext context,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25
+    )
+    {
+        try
+        {
+            var postsCount = await context.Posts.AsNoTracking().CountAsync();
+
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .Where(x => x.Category.Slug == categorySlug)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = postsCount,
+                page,
+                totalPages = postsCount / pageSize,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResultViewModel<List<Post>>("05X4 - Erro inesperado no servidor"));
+        }
+    }
 }
